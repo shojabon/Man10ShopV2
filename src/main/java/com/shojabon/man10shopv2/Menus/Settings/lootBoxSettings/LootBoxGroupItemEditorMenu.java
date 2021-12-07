307dc85a220eaa5827141cd66c50c360bb6a3bda
sho <shojabon@gmail.com>
2021-12-07 09:20:03 +0900

    finished item setting

--- src/main/java/com/shojabon/man10shopv2/Menus/Settings/lootBoxSettings/LootBoxGroupItemEditorMenu.java
+++ src/main/java/com/shojabon/man10shopv2/Menus/Settings/lootBoxSettings/LootBoxGroupItemEditorMenu.java
@@ -1,14 +1,19 @@
 package com.shojabon.man10shopv2.Menus.Settings.lootBoxSettings;
 
+import ToolMenu.ConfirmationMenu;
 import ToolMenu.LargeSInventoryMenu;
+import ToolMenu.NumericInputMenu;
 import ToolMenu.SingleItemStackSelectorMenu;
 import com.shojabon.man10shopv2.DataClass.Man10Shop;
 import com.shojabon.man10shopv2.DataClass.lootBox.LootBox;
+import com.shojabon.man10shopv2.DataClass.lootBox.LootBoxGroupData;
 import com.shojabon.man10shopv2.Man10ShopV2;
 import com.shojabon.mcutils.Utils.SInventory.SInventoryItem;
 import com.shojabon.mcutils.Utils.SItemStack;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
@@ -32,11 +37,15 @@ public class LootBoxGroupItemEditorMenu extends LargeSInventoryMenu {
 
     public void renderMenu(){
         ArrayList<SInventoryItem> items = new ArrayList<>();
+        LootBoxGroupData groupData = lootBox.groupData.get(groupId);
 
-        for(String itemHash: lootBox.groupData.get(groupId).itemCountDictionary.keySet()){
+        for(String itemHash: groupData.itemCountDictionary.keySet()){
 
             SItemStack icon = new SItemStack(lootBox.itemDictionary.get(itemHash).clone());
             //warning
+            float percentage = (float) groupData.itemCountDictionary.get(itemHash)/groupData.getTotalItemCount();
+            icon.addLore("§d§l" + (percentage*100) + "%");
+            icon.addLore("§d§l" + groupData.itemCountDictionary.get(itemHash) + "/" + groupData.getTotalItemCount());
             icon.addLore("");
             icon.addLore("§c§l右クリックで削除");
             icon.addLore("§a§l左クリックで確率設定");
@@ -44,6 +53,43 @@ public class LootBoxGroupItemEditorMenu extends LargeSInventoryMenu {
             //icon.addLore("§c§l右クリックで削除");
             SInventoryItem item = new SInventoryItem(icon.build());
             item.clickable(false);
+
+            item.setAsyncEvent(e -> {
+                if(e.getClick() != ClickType.RIGHT) return;
+                ConfirmationMenu menu = new ConfirmationMenu("グループを消去しますか？", plugin);
+                menu.setOnConfirm(ee -> {
+                    groupData.itemCountDictionary.remove(itemHash);
+                    if(!shop.lootBoxFunction.setLootBox(lootBox)){
+                        player.sendMessage(Man10ShopV2.prefix + "§c§l内部エラーが発生しました");
+                        return;
+                    }
+                    menu.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin));
+                });
+                menu.setOnCancel(ee -> {menu.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin));});
+                menu.setOnCloseEvent(ee -> {menu.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin));});
+                moveToMenu(player, menu);
+            });
+
+            item.setAsyncEvent(e -> {
+                if(e.getClick() != ClickType.LEFT) return;
+
+                NumericInputMenu numberMenu = new NumericInputMenu("アイテム個数を入力してください", plugin);
+                numberMenu.setAllowZero(false);
+                numberMenu.setDefaultValue(groupData.itemCountDictionary.get(itemHash));
+
+                numberMenu.setOnConfirm(ee -> {
+                    groupData.itemCountDictionary.put(itemHash, ee);
+                    if(!shop.lootBoxFunction.setLootBox(lootBox)){
+                        player.sendMessage(Man10ShopV2.prefix + "§c§l内部エラーが発生しました");
+                        return;
+                    }
+                    moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin));
+                });
+                numberMenu.setOnCancel(ee -> numberMenu.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin)));
+                numberMenu.setOnCloseEvent(ee -> numberMenu.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin)));
+
+                moveToMenu(player, numberMenu);
+            });
 
             items.add(item);
         }
@@ -53,11 +99,12 @@ public class LootBoxGroupItemEditorMenu extends LargeSInventoryMenu {
 
     public void afterRenderMenu() {
         renderInventory(0);
+        LootBoxGroupData groupData = lootBox.groupData.get(groupId);
 
         SInventoryItem addPrice = new SInventoryItem(new SItemStack(Material.DROPPER).setDisplayName("§a§lアイテム種を追加").build()).clickable(false);
         addPrice.setAsyncEvent(e -> {
 
-            if(lootBox.groupData.get(groupId).itemCountDictionary.size() >= 45){
+            if(groupData.itemCountDictionary.size() >= 45){
                 player.sendMessage(Man10ShopV2.prefix + "§c§l45アイテム種以上は追加できません");
                 return;
             }
@@ -67,13 +114,13 @@ public class LootBoxGroupItemEditorMenu extends LargeSInventoryMenu {
             itemSelector.selectMaterial(false);
             itemSelector.setOnConfirm(selectedItem -> {
                 SItemStack item = new SItemStack(selectedItem);
-                if(lootBox.groupData.get(groupId).itemCountDictionary.containsKey(item.getMD5())){
+                if(groupData.itemCountDictionary.containsKey(item.getMD5())){
                     player.sendMessage(Man10ShopV2.prefix + "§c§lこのアイテム種はすでに登録されています");
                     itemSelector.moveToMenu(player, new LootBoxGroupItemEditorMenu(player, shop, lootBox, groupId, plugin));
                     return;
                 }
                 lootBox.itemDictionary.put(item.getMD5(), selectedItem);
-                lootBox.groupData.get(groupId).itemCountDictionary.put(item.getMD5(), 1);
+                groupData.itemCountDictionary.put(item.getMD5(), 1);
 
                 //update database here
                 if(!shop.lootBoxFunction.setLootBox(lootBox)){
